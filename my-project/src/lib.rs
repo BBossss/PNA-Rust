10a32399e908d9702c8c61d93409e8f30ae840a9
BBossss <1551929492@qq.com>
2022-05-25 06:16:35 -0700

    implement set get rm

--- my-project/src/lib.rs
+++ my-project/src/lib.rs
@@ -1,6 +1,8 @@
-use std::{collections::HashMap, path::PathBuf, io};
-
+use std::collections::{BTreeMap, HashMap};
+use std::{path::PathBuf, ops::Range, fs::File};
+use std::io::{self, Read, Seek, BufReader, SeekFrom, Write, BufWriter};
 use failure::Fail;
+use serde::{Deserialize, Serialize};
 
 /// The `KvStore` stores string key/value pairs.
 ///
@@ -15,39 +17,73 @@ use failure::Fail;
 /// let val = store.get("key".to_owned());
 /// assert_eq!(val, Some("value".to_owned()));
 /// ```
-#[derive(Default)]
 pub struct KvStore {
-    hashmap: HashMap<String, String>,
+    // directory for the log and other data.
+    path: PathBuf,
+    // Writer for the current log
+    writer: BufWriterWithPos<File>,
+    current_gen: u64,
+    index: BTreeMap<String, CommandPos>,
+    // map generation number to the file reader.
+    readers: HashMap<u64, BufReaderWithPos<File>>,
 }
 
 impl KvStore {
-    /// Creates a `KvStore`.
-    pub fn new() -> KvStore {
-        Self {
-            hashmap: HashMap::new(),
-        }
-    }
-
     /// Sets the value of the string key to a string.
     ///
     /// Return an error if the value is not written successfully.
     pub fn set(&mut self, key: String, value: String) -> Result<()> {
-        unimplemented!();
+        let cmd = Command::set(key, value);
+        let pos = self.writer.pos;
+        serde_json::to_writer(&mut self.writer, &cmd)?;
+        self.writer.flush()?;
+
+        if let Command::Set { key, .. } = cmd {
+            self.index.insert(key, (self.current_gen, pos..self.writer.pos).into());
+        }
+        
+        Ok(())
     }
 
     /// Get the string value of a given string key.
     ///
     /// Return `None` if the given key does not exist.
-    pub fn get(&mut self, _key: String) -> Result<Option<String>> {
-        unimplemented!();
+    pub fn get(&mut self, key: String) -> Result<Option<String>> {
+        if let Some(cmd_pos) = self.index.get(&key) {
+            let reader = self
+                .readers
+                .get_mut(&cmd_pos.gen)
+                .unwrap();
+            reader.seek(SeekFrom::Start(cmd_pos.pos))?;
+            let cmd_reader = reader.take(cmd_pos.len);
+            if let Command::Set {value, .. } = serde_json::from_reader(cmd_reader)? {
+                Ok(Some(value))
+            } else {
+                Err(KvsError::UnexpectedCommandType)
+            }
+        } else {
+            Ok(None)
+        }
     }
 
     /// Remove a given key.
-    pub fn remove(&mut self, _key: String) -> Result<()> {
-        unimplemented!();
+    pub fn remove(&mut self, key: String) -> Result<()> {
+        if self.index.contains_key(&key) {
+            let cmd = Command::remove(key);
+            serde_json::to_writer(&mut self.writer, &cmd)?;
+            self.writer.flush()?;
+
+            if let Command::Remove { key} = cmd {
+                self.index.remove(&key);
+            }
+            Ok(())
+        } else {
+            Err(KvsError::KeyNotFound)
+        }
+        
     }
 
-    pub fn open(_path: impl Into<PathBuf>) -> Result<KvStore> {
+    pub fn open(path: impl Into<PathBuf>) -> Result<KvStore> {
         unimplemented!();
     }
 }
@@ -59,9 +95,16 @@ pub enum KvsError {
     /// IO error
     #[fail(display = "{}", _0)]
     Io(#[cause] io::Error),
+    /// Serialization or deserialization error.
+    #[fail(display = "{}", _0)]
+    Serde(#[cause] serde_json::Error),
     /// Remove non-existing key error
     #[fail(display = "key not found")]
     KeyNotFound,
+    /// Unexpected command type error.
+    /// It indicated a corrupted log or a program bug.
+    #[fail(display = "Unexpected command type")]
+    UnexpectedCommandType,
 }
 
 impl From<io::Error> for KvsError {
@@ -70,5 +113,109 @@ impl From<io::Error> for KvsError {
     }
 }
 
+impl From<serde_json::Error> for KvsError {
+    fn from(err: serde_json::Error) -> KvsError {
+        KvsError::Serde(err)
+    }
+}
+
 /// Result type for Kvstore
-type Result<T> = std::result::Result<T, KvsError>;
+pub type Result<T> = std::result::Result<T, KvsError>;
+
+/// Struct representing a command.
+#[derive(Debug, Serialize, Deserialize)]
+enum Command {
+    Set { key: String, value: String },
+    Remove { key: String },
+}
+
+impl Command {
+    fn set(key: String, value: String) -> Command {
+        Command::Set { key, value }
+    }
+
+    fn remove(key: String) -> Command {
+        Command::Remove { key }
+    }
+}
+
+/// Represents the position and length of a json-serialized command in the log.
+struct CommandPos {
+    gen: u64,
+    pos: u64,
+    len: u64,
+}
+
+impl From<(u64, Range<u64>)> for CommandPos {
+    fn from((gen, range): (u64, Range<u64>)) -> Self {
+        CommandPos {
+            gen: gen,
+            pos: range.start,
+            len: range.end - range.start,
+        }
+    }
+}
+
+struct BufReaderWithPos<R: Read + Seek> {
+    reader: BufReader<R>,
+    pos: u64
+}
+
+impl<R: Read + Seek> BufReaderWithPos<R> {
+    fn new(mut inner: R) -> Result<Self> {
+        let pos = inner.seek(SeekFrom::Current(0))?;
+        Ok(Self {
+            reader: BufReader::new(inner),
+            pos,
+        })
+    }
+}
+
+impl<R: Read + Seek> Read for BufReaderWithPos<R> {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        let len = self.reader.read(buf)?;
+        self.pos += len as u64;
+        Ok(len)
+    }
+}
+
+impl<R: Read + Seek> Seek for BufReaderWithPos<R> {
+    fn seek(&mut self, pos: SeekFrom) -> io::Result<u64> {
+        self.pos = self.reader.seek(pos)?;
+        Ok(self.pos)
+    }
+}
+
+struct BufWriterWithPos<W: Write + Seek> {
+    writer: BufWriter<W>,
+    pos: u64,
+}
+
+impl<W: Write + Seek> BufWriterWithPos<W> {
+    fn new(mut inner: W) -> Result<Self> {
+        let pos = inner.seek(SeekFrom::Current(0))?;
+        Ok(Self {
+            writer: BufWriter::new(inner),
+            pos,
+        })
+    }
+}
+
+impl<W: Write + Seek> Write for BufWriterWithPos<W> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        let len = self.writer.write(buf)?;
+        self.pos += len as u64;
+        Ok(len)
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.writer.flush()
+    }
+}
+
+impl<W: Write + Seek> Seek for BufWriterWithPos<W> {
+    fn seek(&mut self, pos: SeekFrom) -> io::Result<u64> {
+        self.pos = self.writer.seek(pos)?;
+        Ok(self.pos)
+    }
+}
